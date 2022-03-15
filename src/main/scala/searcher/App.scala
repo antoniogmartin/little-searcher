274d428001562b8c7a13b9fab06a7e41508feb4e
Antonio Guzmán <antoniogmartin@correo.ugr.es>
2022-03-15 12:49:32 +0100

    no spark solution

--- src/main/scala/searcher/App.scala
+++ src/main/scala/searcher/App.scala
@@ -1,12 +1,7 @@
 package searcher
 
-import org.apache.spark.mllib.rdd.RDDFunctions.fromRDD
-import org.apache.spark.sql.functions._
-
 import java.io.File
-import org.apache.spark.sql.SparkSession
-
-import scala.collection.immutable.ListMap
+import scala.io.Source
 import scala.io.StdIn.readLine
 
 /**
@@ -31,41 +26,31 @@ object App {
     var sentence = ""
     val files = args.flatMap { dir => getListOfFiles(dir) }
     println(s"${files.length} files read in directory ${args.toList.mkString(",")}\n")
-    val spark = SparkSession.builder()
-      .master("local[1]")
-      .appName("little-search-word-count")
-      .getOrCreate();
-    val sc = spark.sparkContext
 
     while (sentence != ":quit") {
       print("search> ")
       sentence = readLine()
 
       if (sentence != ":quit") {
-        val wordSeq = sentence.toLowerCase().split("""[\s,.;:!?]+""").toSeq
+        val wordSeq = sentence.toLowerCase().trim().split("""[\s,.;:!?*]+""").toSeq
         println("concat arguments = " + wordSeq.toString())
 
-        val result = files.map { f =>
-          val wordCountRDD = sc.textFile(f.getPath)
-          val wordCountArray = wordCountRDD.flatMap(_.split("""[\s,.;:!?]+""")).
-            map(_.toLowerCase).
-            sliding(1).
-            map { case Array(w1) => (w1, 1) }.
-            reduceByKey(_ + _).collect()
-          val wordCount = wordCountArray.filter(x => wordSeq.contains(x._1)) //Also we could extract count of matching words, from wordCount.
+        val filesMap = files.map { f =>
+          val source = Source.fromFile(f.getPath)
 
-          val rank = wordCount.length / (wordSeq.length).toFloat * 100
-
-          (f.toString() -> rank)
-        }.sortBy(_._2)(Ordering[Float].reverse)
-        val documentsWithResults = result.filter(x => x._2 > 0.0)
-        if (documentsWithResults.length != 0)
+          val resultMap = try {
+            val wordList = source.mkString.trim().split("""[\s,.;:!?*]+""").map(_.toLowerCase()).distinct //or toSet
+            val filteredList = wordList.filter(x => wordSeq.contains(x))
+            val rank = filteredList.length / (wordSeq.length).toFloat * 100
+            (f.toString() -> rank)
+          } finally source.close()
+          resultMap
+        }.toMap
+        val documentsWithResults = filesMap.filter(x => x._2 > 0.0).toSeq.sortBy(_._2)(Ordering[Float].reverse)
+        if (documentsWithResults.size != 0)
           println(documentsWithResults.map(x => x._1 + " " + x._2 + "%\n").mkString)
         else print("no matches found\n")
-
       }
-      // wordCountRDD.takeOrdered(10)(Ordering[Int].reverse.on(x=>x._2)).foreach(println)
     }
   }
-
 }
